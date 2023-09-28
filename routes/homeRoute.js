5b40d93c3f6cead1f5795d447888373ffff67c3f
Pedro Henrique <104742078+phpedro06@users.noreply.github.com>
2023-09-28 17:03:25 -0300

    Delete routes directory

--- routes/homeRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const HomeController = require('../controllers/HomeController');
-
-router.get('/', HomeController.homePage);
-
-router.post('/', HomeController.register);
-
-module.exports = router;
